934119efbc297dfd62716466c91af024a1bb438b
Vladimir Strazev <vladimir.strazev@gmail.com>
2024-01-25 23:44:38 +0000

    Added main.py and used functions from funcs.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs import add_numbers, subtract_numbers
+
+result_add = add_numbers(5, 3)
+print(f"Addition result: {result_add}")
+
+result_subtract = subtract_numbers(10, 4)
+print(f"Subtraction result: {result_subtract}")
